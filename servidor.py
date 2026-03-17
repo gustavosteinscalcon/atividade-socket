55603163f2c1a2f371c12bc81063e815abcd1779
gustavoandreS <gustavinhostein11102009@gmail.com>
2026-03-17 15:35:48 -0300

    commit

---
+++ servidor.py
@@ -0,0 +1,13 @@
+# Servidor
+import socket
+
+HOST = "0.0.0.0"
+PORT = 9002
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        data = conn.recv(1024)
+        conn.sendall(b"OK: " + data)
